6dce7fd7ea91c4762437781154ce037b2d866c54
Wallace Breza <wallace@breza.me>
2019-12-19 09:03:59 -0800

    Updated global deployment to use list of endpoints

--- deployment/arm/deploy-global.sh
+++ deployment/arm/deploy-global.sh
@@ -3,36 +3,31 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 
 prefix=$1
-subscriptionId="d36d0808-a967-4f73-9fdc-32ea232fc81d"
-location=$2
-appName=$3
-fullPrefix="$1-$2-$3"
-regionalResourceGroupName="$fullPrefix-rg"
-globalResourceGroupName="$prefix-gbl-$appName-rg"
-
-echo "Resource Group: $regionalResourceGroupName"
-echo "Prefix: $prefix"
-echo "Location: $location"
-echo "App Name: $appName"
-echo "Prefix: $fullPrefix"
-
+appName=$2
+frontendHosts=$3
+backendHosts=$4
+resourceGroupName="$prefix-gbl-$appName-rg"
 frontDoorName="$prefix-gbl-$appName-fd"
-apimName="$fullPrefix-apim"
-frontendAppName="$fullPrefix-frontend"
-backendAppName="$fullPrefix-backend"
+
+echo "Resource Group: $resourceGroupName"
+echo "Prefix: $prefix"
+echo "App Name: $appName"
+echo "Front Door: $frontDoorName"
+echo "Frontend Hosts: $frontendHosts"
+echo "Backend Hosts: $backendHosts"
 
 timestamp() {
   date +"%Y%m%dZ%H%M%S"
 }
 
-echo "Creating global resource Group: $globalResourceGroupName"
+echo "Creating global resource Group: $resourceGroupName"
 az group create \
-  --name $globalResourceGroupName \
+  --name $resourceGroupName \
   --location centralus
 
-echo "Deploying global resources to $globalResourceGroupName"
+echo "Deploying global resources to $resourceGroupName"
 az group deployment create \
   --name "Urlist-global-$(timestamp)" \
-  --resource-group $globalResourceGroupName \
+  --resource-group $resourceGroupName \
   --template-file global.json \
-  --parameters "frontDoorName=$frontDoorName apimName=$apimName frontendAppName=$frontendAppName backendAppName=$backendAppName"
+  --parameters "frontDoorName=$frontDoorName"
--- deployment/arm/deploy.sh
+++ deployment/arm/deploy.sh
@@ -2,6 +2,9 @@
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 
+frontendHosts='("wabrez-westus2-frontend.azurewebsites.net","wabrez-eastus-frontend.azurewebsites.net")'
+backendendHosts='("wabrez-westus2-apim.azure-api.net","wabrez-eastus-apim.azure-api.net")'
+
 bash deploy-region.sh wabrez westus2 urlist
 bash deploy-region.sh wabrez eastus urlist
-bash deploy-global.sh wabrez westus2 urlist
+bash deploy-global.sh wabrez urlist $frontendHosts $backendendHosts

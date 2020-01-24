7abf136576b486c8c8ee652b6b43b5b468377452
Wallace Breza <wallace@breza.me>
2020-01-23 16:26:57 -0800

    test: Adds integration tests for Links API (#32)
    Adds a new integration tests project to run integration tests for backend APIs.
    
     Adds integration tests for Links API
     Updates API pipeline to only run unit test project
     Updates API pipeline to deploy to staging slot and auto swap after running integration tests.

---
+++ api/tests/LinkyLink.Integration.Tests/AzureAdB2C.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+
+namespace LinkyLink.Integration.Tests
+{
+    /// <summary>
+    /// Azure AD B2C Configuration for Integration tests
+    /// </summary>
+    [JsonObject]
+    public class AzureAdB2C
+    {
+        [JsonProperty]
+        public string Authority { get; set; }
+        [JsonProperty]
+        public string BaseAddress { get; set; }
+        [JsonProperty]
+        public string Username { get; set; }
+        [JsonProperty]
+        public string Password { get; set; }
+        [JsonProperty]
+        public string Scope { get; set; }
+        [JsonProperty]
+        public string ClientId { get; set; }
+    }
+}

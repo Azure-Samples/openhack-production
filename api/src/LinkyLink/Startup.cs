10f25eb6013081d91b6182bdbccdaf3b0e188d01
Hussain <humoiz@microsoft.com>
2020-02-06 15:36:34 -0800

    removed duplicate App Insights from startup

--- api/src/LinkyLink/Startup.cs
+++ api/src/LinkyLink/Startup.cs
@@ -83,9 +83,6 @@ namespace LinkyLink
                 var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
                 c.IncludeXmlComments(xmlPath);
             });
-            
-            // Add Application Insights Telemetry
-            services.AddApplicationInsightsTelemetry();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

e03c8445c29b0ce8da806c465799992acafe628b
Wallace Breza <wallace.breza@microsoft.com>
2020-02-25 18:19:03 +0000

    Merged PR 1961: Improves test coverage for LinkService
    Improves test coverage for LinkService

--- api/tests/LinkyLink.Tests/Service/LinksServiceTests.cs
+++ api/tests/LinkyLink.Tests/Service/LinksServiceTests.cs
@@ -33,10 +33,19 @@ namespace LinkyLink.Tests.Service
         {
             _sourceList = new List<LinkBundle>();
             _mockSourceList = _sourceList.AsQueryable().BuildMockDbSet();
+            _mockSourceList
+                .Setup(m => m.Add(It.IsAny<LinkBundle>()))
+                .Callback<LinkBundle>(bundle => _sourceList.Add(bundle));
+
+            _mockSourceList
+                .Setup(m => m.Remove(It.IsAny<LinkBundle>()))
+                .Callback<LinkBundle>(bundle => _sourceList.Remove(bundle));
+
             _httpContextAccessor = new HttpContextAccessor();
             _mockUserAuth = new Mock<UserAuth>(_httpContextAccessor);
             _mockLinksContext = new Mock<LinksContext>(new DbContextOptionsBuilder<LinksContext>().Options, new ConfigurationBuilder().Build());
             _mockLinksContext.Setup(m => m.LinkBundle).Returns(_mockSourceList.Object);
+
             _linksService = new LinksService(_mockLinksContext.Object, _mockUserAuth.Object);
         }
 
@@ -44,44 +53,36 @@ namespace LinkyLink.Tests.Service
         public async Task CreateLinkBundleAsyncCreatesLinkBundleInDB()
         {
             // Arrange
-            var linkBundles = new List<LinkBundle>
-            {
-                new LinkBundle { VanityUrl = "samplelink" }
-            };
+            var newBundle = new LinkBundle { VanityUrl = "samplelink" };
+            Assert.Equal(0, _sourceList.Count());
 
-            var MockSet = new Mock<DbSet<LinkBundle>>();
-            MockSet.Setup(m => m.Add(It.IsAny<LinkBundle>())).Callback<LinkBundle>((entity) => linkBundles.Add(entity));
-            _mockLinksContext.Setup(x => x.LinkBundle).Returns(MockSet.Object);
+            // Act
+            await _linksService.CreateLinkBundleAsync(newBundle);
 
-            await _linksService.CreateLinkBundleAsync(new LinkBundle
-            {
-                VanityUrl = "samplelink1"
-            });
-
-            Assert.Equal(2, linkBundles.Count);
+            // Assert
+            Assert.Equal(1, _sourceList.Count());
         }
 
         [Fact]
         public async Task RemoveLinkBundleAsyncRemovesLinkBundleFromDB()
         {
             // Arrange
-            var linkBundles = new List<LinkBundle>
-            {
-                new LinkBundle { Id = "samplelink" }
-            };
+            var bundleToDelete = new LinkBundle { Id = "samplelink" };
+            _sourceList.Add(bundleToDelete);
+            Assert.Equal(1, _sourceList.Count);
 
-            var MockSet = new Mock<DbSet<LinkBundle>>();
-            MockSet.Setup(m => m.Remove(It.IsAny<LinkBundle>())).Callback<LinkBundle>((entity) => linkBundles.Remove(entity));
-            _mockLinksContext.Setup(x => x.LinkBundle).Returns(MockSet.Object);
+            // Act
+            await _linksService.RemoveLinkBundleAsync(bundleToDelete);
 
-            await _linksService.RemoveLinkBundleAsync(linkBundles[0]);
 
-            Assert.Equal(0, linkBundles.Count);
+            //Asert
+            Assert.Equal(0, _sourceList.Count);
         }
 
         [Fact]
-        public async Task GetAllLinkBundlesWithPaging()
+        public async Task GetAllLinkBundlesWithExplicitPaging()
         {
+            // Arrange
             for (var i = 1; i <= 100; i++)
             {
                 _sourceList.Add(new LinkBundle
@@ -92,6 +93,7 @@ namespace LinkyLink.Tests.Service
                 });
             }
 
+            // Act
             var queryOptions = new QueryOptions
             {
                 Skip = 10,
@@ -99,8 +101,161 @@ namespace LinkyLink.Tests.Service
             };
 
             var results = (await _linksService.AllLinkBundlesAsync(queryOptions)).ToList();
+
+            // Assert
             Assert.Equal(queryOptions.Top, results.Count());
             Assert.Equal("Link Bundle 11", results[0].Description);
+        }
+
+        [Fact]
+        public async Task GetAllLinkBundlesWithDefaultPaging()
+        {
+            // Arrange
+            for (var i = 1; i <= 100; i++)
+            {
+                _sourceList.Add(new LinkBundle
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Description = $"Link Bundle {i}",
+                    VanityUrl = $"link-bundle-{i}"
+                });
+            }
+
+            // Act
+            var results = (await _linksService.AllLinkBundlesAsync()).ToList();
+
+            // Assert
+            Assert.Equal(QueryOptions.Default.Top, results.Count());
+            Assert.Equal("Link Bundle 1", results[0].Description);
+        }
+
+        [Fact]
+        public async Task FindLinkBundleWithMatchingVanityUrl()
+        {
+            // Arrange
+            for (var i = 1; i <= 100; i++)
+            {
+                _sourceList.Add(new LinkBundle
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Description = $"Link Bundle {i}",
+                    VanityUrl = $"link-bundle-{i}"
+                });
+            }
+
+            var random = new Random(DateTime.UtcNow.Millisecond);
+            var index = random.Next(0, 99);
+            var vanityUrl = _sourceList[index].VanityUrl;
+
+            // Act
+            var match = await _linksService.FindLinkBundleAsync(vanityUrl);
+
+            // Assert
+            Assert.NotNull(match);
+            Assert.Equal(vanityUrl, match.VanityUrl);
+        }
+
+        [Fact]
+        public async Task FindLinkBundleWithoutMatchingVanityUrl()
+        {
+            // Arrage
+
+            // Act
+            var match = await _linksService.FindLinkBundleAsync(Guid.NewGuid().ToString());
+
+            // Assert
+            Assert.Null(match);
+        }
+
+        [Fact]
+        public async Task FindLinkBundleExistsReturnsTrue()
+        {
+            // Arrange
+            for (var i = 1; i <= 100; i++)
+            {
+                _sourceList.Add(new LinkBundle
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Description = $"Link Bundle {i}",
+                    VanityUrl = $"link-bundle-{i}"
+                });
+            }
+
+            var random = new Random(DateTime.UtcNow.Millisecond);
+            var index = random.Next(0, 99);
+
+            // Act
+            var exists = await _linksService.LinkBundleExistsAsync(_sourceList[index].Id);
+
+            // Assert
+            Assert.True(exists);
+        }
+
+        [Fact]
+        public async Task FindLinkBundleExistsReturnsFalse()
+        {
+            // Arrange
+
+            // Act
+            var exists = await _linksService.LinkBundleExistsAsync(Guid.NewGuid().ToString());
+
+            // Assert
+            Assert.False(exists);
+        }
+
+        [Fact]
+        public async Task FindLinkBundlesForUserWithExplicitPaging()
+        {
+            // Arrange
+            var userId = "user@contoso.com";
+
+            for (var i = 1; i <= 100; i++)
+            {
+                _sourceList.Add(new LinkBundle
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    UserId = userId,
+                    Description = $"Link Bundle {i}",
+                    VanityUrl = $"link-bundle-{i}"
+                });
+            }
+
+            // Act
+            var queryOptions = new QueryOptions
+            {
+                Skip = 10,
+                Top = 10
+            };
+
+            var results = (await _linksService.FindLinkBundlesForUserAsync(userId, queryOptions)).ToList();
+
+            // Assert
+            Assert.Equal(queryOptions.Top, results.Count());
+            Assert.Equal("Link Bundle 11", results[0].Description);
+        }
+
+        [Fact]
+        public async Task FindLinkBundlesForUserWithDefaultPaging()
+        {
+            // Arrange
+            var userId = "user@contoso.com";
+            for (var i = 1; i <= 100; i++)
+            {
+                _sourceList.Add(new LinkBundle
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    UserId = userId,
+                    Description = $"Link Bundle {i}",
+                    VanityUrl = $"link-bundle-{i}"
+                });
+            }
+
+            // Act
+            var results = (await _linksService.FindLinkBundlesForUserAsync(userId)).ToList();
+
+            // Assert
+            Assert.Equal(QueryOptions.Default.Top, results.Count());
+            Assert.Equal("Link Bundle 1", results[0].Description);
         }
     }
 }
